95df19b279819cf0c79842c80ccf0e901fde3f49
lisen <xiaojiehao123@gmail.com>
2023-09-08 15:33:12 +0800

    add tcx script

---
+++ tcx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+npm start -- -u xiaojiehao123@gmail.com -p altercoros123 -o .
+# FILE_NAME=`npm start -- -u xiaojiehao123@gmail.com -p altercoros123 -o .`
+# echo "TEST"
+# echo ${FILE_NAME}
