2d56c70130cc33865dc05b758413a72e5291b9ce
lisen <xiaojiehao123@gmail.com>
2023-09-08 17:02:12 +0800

    loop execute

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sh tcx.sh
-python3 /root/running_page/running_page/scripts/tcx_sync.py
+while true; do
+  sh tcx.sh
+  python3 /root/running_page/running_page/scripts/tcx_sync.py
+  sleep 3m
+done
