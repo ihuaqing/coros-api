656b23b21d56921517fe6e781ac8e365fa2c944b
lisen <xiaojiehao123@gmail.com>
2023-09-08 16:20:14 +0800

    add object dir

--- tcx.sh
+++ tcx.sh
@@ -2,6 +2,7 @@
 
 LABEL_ID=`node --loader ts-node/esm src/index.ts -u xiaojiehao123@gmail.com -p altercoros123 -o .`
 SOURCE_TCX=${LABEL_ID}.tcx
+OBJECT_DIR="../"
 
 # Delete old file, include tcx.1 style
 rm ${SOURCE_TCX}*
@@ -11,7 +12,7 @@ wget https://oss.coros.com/tcx/451856893611098112/${SOURCE_TCX}
 OBJECT_TCX=`python3 tcx.py ${SOURCE_TCX}`
 
 if [ ! -f "${OBJECT_TCX}" ];then
-  mv ${SOURCE_TCX} ${OBJECT_TCX}
+  mv ${SOURCE_TCX} ${OBJECT_DIR}${OBJECT_TCX}
 else
   echo "file existed"
   rm ${SOURCE_TCX}
